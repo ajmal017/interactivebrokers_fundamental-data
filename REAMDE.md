4675780c7610441edf9c34134924ad8ed4a91eec
Felix Baron <45421716+felixbaron@users.noreply.github.com>
2020-10-13 09:26:11 +0200

    add connect script

---
+++ REAMDE.md
@@ -0,0 +1,7 @@
+# Interactive Brokers Connector
+
+This repository contains helpful snippets to connect to [Interactive Brokers](https://www.interactivebrokers.com).
+
+## Installation
+
+- Install [IB Gateway](https://www.interactivebrokers.eu/de/?f=16457).
--- ib_insync_sample.py
+++ ib_insync_sample.py
@@ -1,1 +1,17 @@
 # TODO: Fetch data with ib_insync, see https://github.com/erdewit/ib_insync
+
+from ib_insync import IB, Forex, util
+util.startLoop()  # uncomment this line when in a notebook
+
+ib = IB()
+ib.connect('127.0.0.1', 4001, clientId=2)
+
+contract = Forex('EURUSD')
+bars = ib.reqHistoricalData(
+    contract, endDateTime='', durationStr='30 D',
+    barSizeSetting='1 hour', whatToShow='MIDPOINT', useRTH=True)
+
+# convert to pandas dataframe:
+df = util.df(bars)
+print(df)
+ib.disconnect()
