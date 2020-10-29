ec27e85d448708e7d5a5e390ff1343b2af3d6725
Felix Baron <45421716+felixbaron@users.noreply.github.com>
2020-10-29 05:19:46 +0100

    add spark operation

--- ib_insync_sample.py
+++ ib_insync_sample.py
@@ -14,4 +14,23 @@ bars = ib.reqHistoricalData(
 # convert to pandas dataframe:
 df = util.df(bars)
 print(df)
+
 ib.disconnect()
+
+from pyspark.sql import SparkSession
+import os
+# warehouseLocation = 'hdfs://localhost:9000/warehouse'
+warehouseLocation = 'hdfs://localhost:8020/user/hive/warehouse'
+os.environ["HADOOP_USER_NAME"] = "root"
+spark = SparkSession.builder \
+    .appName('appName') \
+    .config('spark.driver.port', 8080) \
+    .config("spark.sql.warehouse.dir", warehouseLocation) \
+    .config('spark.driver.host', 'http://127.0.0.1:58896/api/v1/namespaces/default/services/spark:spark-master-6d4984666-s68v2:8080/proxy/#/') \
+    .config('spark.sql.hive.thriftServer.singleSession', True) \
+    .enableHiveSupport() \
+    .getOrCreate()
+spark
+spark.sql("CREATE TABLE IF NOT EXISTS src (key INT, value STRING) USING hive")
+
+spark.sql("LOAD DATA LOCAL INPATH 'sample.json' INTO TABLE src")
